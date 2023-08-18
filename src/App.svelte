b5ef75b647e12f3ddf5bb21a2a2a53eb2220acfa
BaZZcheesy <107484617+BaZZcheesy@users.noreply.github.com>
2023-08-18 16:00:42 +0200

    Uploading src folder

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  let name = "Ian";
+  let age = 18
+
+
+const increaseAge = () => {
+  age++;
+}
+
+const decreaseAge = () => {
+  age--;
+}
+
+</script>
+
+<main>
+  <h1>Svelte</h1>
+
+  <p>{name}</p>
+  <p>{age}</p>
+  <button on:click={decreaseAge}>Decrease Age</button>
+  <button on:click={increaseAge}>Increase Age</button>
+</main>
